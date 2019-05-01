173e6e1ea12a7579692dde0dfcadd6cfc1506d2c
ATai2 <1114677814@qq.com>
2019-05-01 22:38:54 +0800

    add windows gui actions

---
+++ mycnote/main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<Windows.h>
+
+void openNotepad() {
+	system("\"C:\\Program Files\\Notepad++\\notepad++.exe\"");
+}
+
+void closeNotePad() {
+	printf("start to close notepad");
+	system("taskkill /f /im notepad++.exe");
+
+}
+
+void test() {
+	ShellExecuteA(0,"open","calc",0,0,1);
+}
+
+int main() {
+	printf("helloworld");
+	openNotepad();
+
+	Sleep(5000);
+	closeNotePad();
+	test();
+	return 0;
+}
